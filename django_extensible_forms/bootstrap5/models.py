69b49829c2a7d7ed493112879c0363af5c4a3123
Naggafin <7529169+Naggafin@users.noreply.github.com>
2023-11-05 17:02:25 -0500

    fixed issue with UsernameField erroring while using non-SQLite db

--- django_extensible_forms/bootstrap5/models.py
+++ django_extensible_forms/bootstrap5/models.py
@@ -1,49 +1,46 @@
-from ..models import (
-	BaseExtensibleModelForm,
-	ExtensibleModelFormMetaclass,
-	ModelChoiceField as ExtensibleModelChoiceField,
-	ModelMultipleChoiceField as ExtensibleModelMultipleChoiceField,
-	extensible_formfield_callback,
-	modelform_factory as extensible_modelform_factory,
-)
+from ..models import BaseExtensibleModelForm, ExtensibleModelFormMetaclass
+from ..models import ModelChoiceField as ExtensibleModelChoiceField
+from ..models import ModelMultipleChoiceField as ExtensibleModelMultipleChoiceField
+from ..models import extensible_formfield_callback
+from ..models import modelform_factory as extensible_modelform_factory
 from .fields import Bootstrap5FieldMixin, _monkey_patch_field
 from .forms import Bootstrap5FormMetaclass
 
 __all__ = (
-	"Bootstrap5ModelFormMetaclass",
-	"ModelForm",
-	"ModelChoiceField",
-	"ModelMultipleChoiceField",
+    "Bootstrap5ModelFormMetaclass",
+    "ModelForm",
+    "ModelChoiceField",
+    "ModelMultipleChoiceField",
 )
 
 
 def bootstrap5_formfield_callback(field, **kwargs):
-	formfield = extensible_formfield_callback(field, **kwargs)
-	return _monkey_patch_field(formfield)
+    formfield = extensible_formfield_callback(field, **kwargs)
+    return _monkey_patch_field(formfield)
 
 
 class Bootstrap5ModelFormMetaclass(
-	Bootstrap5FormMetaclass, ExtensibleModelFormMetaclass
+    Bootstrap5FormMetaclass, ExtensibleModelFormMetaclass
 ):
-	formfield_callback = bootstrap5_formfield_callback
+    formfield_callback = bootstrap5_formfield_callback
 
-	def bases_check(mcs, bases):
-		return bases == (BaseExtensibleModelForm,)
+    def bases_check(mcs, bases):
+        return bases == (BaseExtensibleModelForm,)
 
 
 class ModelForm(BaseExtensibleModelForm, metaclass=Bootstrap5ModelFormMetaclass):
-	pass
+    pass
 
 
 class ModelChoiceField(Bootstrap5FieldMixin, ExtensibleModelChoiceField):
-	pass
+    pass
 
 
 class ModelMultipleChoiceField(
-	Bootstrap5FieldMixin, ExtensibleModelMultipleChoiceField
+    Bootstrap5FieldMixin, ExtensibleModelMultipleChoiceField
 ):
-	pass
+    pass
 
 
 def modelform_factory(*args, form=ModelForm, **kwargs):
-	return extensible_modelform_factory(*args, form=form, **kwargs)
+    return extensible_modelform_factory(*args, form=form, **kwargs)
--- django_extensible_forms/tests.py
+++ django_extensible_forms/tests.py
@@ -6,17 +6,17 @@ from django.forms.fields import Field
 from .forms import Form
 
 TestFormClass = type(
-	"TestForm",
-	(Form,),
-	{
-		name: getattr(fields, name)()
-		for name in fields.__all__
-		if isinstance(getattr(fields, name), Field)
-	},
+    "TestForm",
+    (Form,),
+    {
+        name: getattr(fields, name)()
+        for name in fields.__all__
+        if isinstance(getattr(fields, name), Field)
+    },
 )
 
 
 class FormTests(TestCase):
-	def test_form_rendition(self):
-		form = TestFormClass()
-		self.assertIsInstance(str(form), str)
+    def test_form_rendition(self):
+        form = TestFormClass()
+        self.assertIsInstance(str(form), str)
